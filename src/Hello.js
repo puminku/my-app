de6a57475444f620583d9272f1c4ffb6f8f1ab62
Pumin Suksuwan <pumin.su@live.ku.th>
2023-04-26 15:46:32 +0700

    First Version React 101

---
+++ src/Hello.js
@@ -0,0 +1,10 @@
+import './Hello.css';
+const Hello = ({children}) =>{
+    return ( <>
+        <h1>Hello World 
+            {children} 
+        </h1>
+        </>);
+}
+
+export default Hello;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+//import App from './App';
+import Hello from './Hello';
+import Profile from './Profile';
+//import Counter from './Counter';
+import reportWebVitals from './reportWebVitals';
+import PackingList from './PackingList';
+import Bmi from './Bmi';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Bmi />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
